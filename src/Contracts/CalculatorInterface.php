8eddd79fa4300a4b285e66304411714492cc77a1
Mehmet ÖĞMEN <e-posta@mehmetogmen.com.tr>
2020-10-14 00:32:50 +0300

    CalcutorInterface modified.

--- src/Contracts/CalculatorInterface.php
+++ src/Contracts/CalculatorInterface.php
@@ -6,13 +6,4 @@ use ValueObjects\Metrology\Contracts\UnitTypes\Temperature\TemperatureInterface;
 
 interface CalculatorInterface
 {
-    public function add(float $value): TemperatureInterface;
-
-    public function subtract(float $value): TemperatureInterface;
-
-    public function multiply(float $value): TemperatureInterface;
-
-    public function divide(float $value): TemperatureInterface;
-
-    public function mod(float $value): TemperatureInterface;
 }
